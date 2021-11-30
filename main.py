ac84f6f9c30c048faa4bb7bbae1c62406a7b71e5
Lewis <lewisk1899@email.arizona.edu>
2021-11-29 23:03:00 -0700

    Implementation of some sort of mock malicious pdf

---
+++ main.py
@@ -0,0 +1,29 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+def read_pdf():
+    filename = "Homework01.pdf"
+    pdfFileObject = open(filename, 'rb')
+    pdfReader = PdfFileReader(pdfFileObject)
+    pageObject = pdfReader.getPage(0)
+    print(pageObject.extractText())
+    pdfFileObject.close()
+
+def convert_to_malicious_pdf():
+    filename = "Homework01.pdf"
+    output = PdfFileWriter()
+    input_pdf = PdfFileReader(open(filename, 'rb'))
+    # go through page by page
+    for page_number in range(input_pdf.getNumPages()):
+        page = input_pdf.getPage(page_number)
+        output.addPage(page)
+
+    with open("MaliciousHomework1.pdf", 'wb') as file:
+        output.addJS("this.print({bUI:true,bSilent:false,bShrinkToFit:true});")
+        output.write(file)
+
+
+def main():
+    read_pdf()
+    convert_to_malicious_pdf()
+
+main()
